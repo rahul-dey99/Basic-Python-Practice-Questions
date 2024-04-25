5e44f959249151abe776e7025e519f52a3fa3869
rahul-dey99 <rahul.dey.741787@gmail.com>
2024-04-25 21:24:59 +0530

    third commint

---
+++ p131_to140.py
@@ -0,0 +1,41 @@
+# Q.131
+# name = "Rahul"
+# intro = f"My name is {name}"   # Using f string to format string to support variable in middle.
+# print(intro)
+
+# Q.132
+# name = input("Enter Name:")   # input() function can take user input on the terminal.
+# print("Your name is", name)
+
+# Q.133
+# str = "KOKO"
+# str = str.lower()     # Using lower() method we can convert a string to all lowercase.
+# print(str)
+
+# Q.134
+# = is an assignment operator which is used to assisgn values to varibales.
+# == is a logical operator which is used to check is 2 values are equal.
+
+# Q.135
+# dict = {'a':11, 'b':12}
+# x = dict.get('c')      # The get() method of dictionary can be used to access a specific key. 
+# print(x)               # If key is present it returns the key otherwise it returns None.
+
+# Q.136
+# print(list("hello"))  # It converts each character into an element of a list.
+
+# Q.137
+# s1 = {1,2,4}        # Set with elements.
+# s2 = set()          # Empty set.
+# print(type(s1), type(s2))
+
+# Q.138
+# print(set([1,2,3,4]))   # Converts the list into a set.
+
+# Q.139
+# 0 is the first index in python list.
+
+# Q.140
+# str = "Hello-World"
+# new_str = str[2:6]     # This is how we can get the characters from index 2 to 5 in a string.
+# print(new_str)
