794063912e5df30fb420dc94711b71631226807c
rahul-dey99 <rahul.dey.741787@gmail.com>
2024-05-12 20:21:30 +0530

    Third message

---
+++ p141_to_150.py
@@ -0,0 +1,44 @@
+# Q.141
+# print(max([1,2,3,5]))   ## max() function returns the maximum element of a list.
+
+# Q.142
+# lst = [3,4,5,6,7]
+# tup = tuple(lst)          ## Using the tuple() method we can convert list to tuple.
+# print(type(tup))
+
+# Q.143
+# lst = [3,4,5,6,7]
+# print(lst.index(3))     ## index() function takes in an element and returns the index of the element.
+
+# Q.144
+# print(type(1.0))         ## type of 1.0 is a float.
+
+# Q.145
+# x = 7 
+# print(type(x))             ## we can use the type function to check if a variable is integer or not.
+
+# Q.146
+# for i in range(5):           ## Executing a block of code many times using a for loop.
+#     print("\n")
+#     print("Block Starts....")
+#     print(5*i)
+#     print("Block ends....")
+#     print("\n")
+
+# Q.147
+# A syntax error in python means when the syntax of code is not written properly. 
+# It could occur when indentation is not proper or the misplacement of brackets, etc.
+
+# Q.148
+# print(x)   # We get an NameError: name 'x' is not defined .
+
+# Q.149
+# name = "Rahul Dey"
+# first_name, last_name = name.split(" ")             ## We can use the split function to split a string.
+# print("First name:", first_name, "\nLast name:", last_name)
+
+# Q.150
+# strs = ['Hello', 'World']
+# separator = " "
+# combined_str = separator.join(strs)    ## The join() method can combine list of strings, it takes a separator and a list
+# print(combined_str)                    # and returns a combined string.
---
+++ p151_to_160.py
@@ -0,0 +1,20 @@
+# Q.151
+# a = 'a'
+# print(ord(a))   # The ord() function takes a character and returns its ASCII value.
+
+# Q.152
+# pass
+
+# Q.153
+# pass is used when we want to write code logic for a block later but can not leave it blank
+# otherwise it will throw undefined error. In this case we use pass.
+
+# Q.154
+# import sys                      # import sys imports the sys module which is used to 
+# str = 'faf'
+# size = sys.getsizeof(str)
+# print(size)
+
+# obj = [1, 2, 3, 4, 5]
+# size_in_bytes = sys.getsizeof(obj)
+# print(size_in_bytes)
